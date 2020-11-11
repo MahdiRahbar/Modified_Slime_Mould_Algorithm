651321dc7d46418ecf8a76da350d65e728d31630
Mahdi <mahdirahbar01@gmail.com>
2020-11-11 11:36:50 +0330

    applied a few basic changes

--- MSMA.m
+++ MSMA.m
@@ -129,7 +129,8 @@ while  it <= Max_iter -1
         if rand<z     %Eq.(2.7)
             X(i,:) = (ub-lb)*rand+lb;
         else
-            p =tanh(abs(AllFitness(i)-Destination_fitness));  %Eq.(2.2)
+%             p =tanh(abs(AllFitness(i)-Destination_fitness));  %Eq.(2.2)
+            p = i/N;
             vb = unifrnd(-a,a,1,dim);  %Eq.(2.3)
             vc = unifrnd(-b,b,1,dim);
             for j=1:dim
@@ -137,9 +138,14 @@ while  it <= Max_iter -1
                 A = randi([1,N]);  % two positions randomly selected from population
                 B = randi([1,N]);
                 if r<p    %Eq.(2.1)
-                    X(i,j) = bestPositions(j)+ vb(j)*(weight(i,j)*X(A,j)-X(B,j));
+%                     X(i,j) = bestPositions(j)+ vb(j)*(weight(i,j)*X(A,j)-X(B,j));   % Current to Pbest
+%                     X(i,j) = X(i,j) + (bestPositions(j) - X(i,j)) ;  % + vb(j)*(weight(i,j)*X(A,j)-X(B,j))
+                    X(i,j) = X(i,j) + (weight(i,j)*X(A,j)-X(B,j));
                 else
-                    X(i,j) = vc(j)*X(i,j);
+%                     X(i,j) = vc(j)*X(i,j);  % CLPSO
+                     X(i,j) =  bestPositions(j) + vb(j)*(weight(i,j)*X(A,j)-X(B,j)); 
+
+
                 end
             end
         end
